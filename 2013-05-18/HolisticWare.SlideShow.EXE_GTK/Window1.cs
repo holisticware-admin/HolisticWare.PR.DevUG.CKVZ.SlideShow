3fd4ba79421d04e628bb571aa20e2125d2f6dcee
moljac <moljac@holisticware.com>
2013-05-31 10:12:58 +0200

    GTK# exe app added

---
+++ 2013-05-18/HolisticWare.SlideShow.EXE_GTK/Window1.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace HolisticWare.SlideShow.EXE_GTK
+{
+	public partial class Window1 : Gtk.Window
+	{
+		public Window1 () : 
+				base(Gtk.WindowType.Toplevel)
+		{
+			this.Build ();
+		}
+	}
+}
+
