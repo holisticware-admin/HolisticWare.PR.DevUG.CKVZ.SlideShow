5679b7c5e657d9b56598dfb5a8b2c78d33489da5
moljac <moljac@holisticware.com>
2013-06-10 17:29:59 +0200

    mvc4 done

---
+++ 2013-06-10/HolisticWare.SlideShow/HolisticWare.SlideShow.EXE_ASPnetMVC4_mono/Models/Slide.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HolisticWare.SlideShow.EXE.Models
+{
+	public partial class Slide
+	{
+		public Slide(string path, string desc)
+		{
+			Path = path;
+			Description = desc;
+		}
+		public string Path { get; set; }
+		public string Description { get; set;}
+	}
+}
