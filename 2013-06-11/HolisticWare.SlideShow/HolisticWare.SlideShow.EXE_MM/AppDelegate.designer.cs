d9a23047f36affae8dc3c58a7a041d0b55449f10
Iki <idikic@holisticware.com>
2013-06-11 15:13:24 +0200

    IKI - step 1

---
+++ 2013-06-11/HolisticWare.SlideShow/HolisticWare.SlideShow.EXE_MM/AppDelegate.designer.cs
@@ -0,0 +1,10 @@
+
+namespace HolisticWare.SlideShow.EXE_MM
+{
+	// Should subclass MonoMac.AppKit.NSResponder
+	[MonoMac.Foundation.Register("AppDelegate")]
+	public partial class AppDelegate
+	{
+	}
+}
+
