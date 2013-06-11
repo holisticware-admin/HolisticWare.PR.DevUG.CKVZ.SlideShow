9d7e431c8cb0a22f152dcce53495d210bf2db163
moljac <moljac@holisticware.com>
2013-06-11 05:52:41 +0200

    xaml

--- HolisticWare.Common.Scripts/browse.bat
+++ HolisticWare.Common.Scripts/browse.bat
@@ -11,8 +11,8 @@ set BROWSER="%PROGRAMFILES%\Google\Chrome\Application\chrome.exe"
 	http://mono-aspnet-mvc4-aws.holisticware.org/ ^
 	http://mono-aspnet-mvc4-aws.holisticware.org/WebServiceFileUploader.asmx ^
 	http://holisticware-ft-compositec1.ironfoundry.me/holisticware/layout/presentations/reveal-js/ ^
-
-
+	http://127.0.0.1:8080/ ^
+	http://127.0.0.1:8080/WebServiceFileUploader.asmx ^
 
 
 
@@ -20,4 +20,5 @@ echo ===========================================================================
 
 @IF %ERRORLEVEL% NEQ 0 PAUSE	
 
+:: "%PROGRAMFILES%\Mono-3.0.10\bin\xsp4.bat"
 
